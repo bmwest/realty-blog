1040598203be0b2eaf4fcb3a9ad11930c4ce4c2c
bmwest <brianawest.m@gmail.com>
2017-03-12 18:14:22 -0400

    adds testing for article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:title).when("Next Open House") }
+  it { should_not have_valid(:title).when(nil, "") }
+
+  it { should have_valid(:body).when("Our next open house will be
+                                      in a lovely colonial Chestnut Hill
+                                      mansion. To attend, please RSVP at:
+                                      267-555-5555 by no later than Wednesday,
+                                      April 3, 2017. Hope to see you there!") }
+  it { should_not have_valid(:body).when(nil, "") }
 end
