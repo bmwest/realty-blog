7cef553b837670110e8f6a96f2bfa8f106ef04b2
bmwest <brianawest.m@gmail.com>
2017-03-18 18:26:05 -0400

    adds user nav options

--- spec/features/06_user_sees_article_index_spec.rb
+++ spec/features/06_user_sees_article_index_spec.rb
@@ -2,59 +2,97 @@ require 'rails_helper'
 
 feature "user sees articles" do
   let!(:user) { FactoryGirl.create(:user, role: "member") }
-  let!(:article) { FactoryGirl.create(:article) }
+  let!(:article) { FactoryGirl.create(:article, user: user) }
 
-  xit "a registered user can see all articles" do
+  scenario "a registered user can see a list of all articles" do
+    visit root_path
     sign_in(user)
 
     expect(page).to have_content("Hey, nice to see you again.")
     expect(page).to have_content("View All Articles")
 
     click_link "View All Articles"
-    expect(page).to have_content("Delete #{article.title}")
-
-    click_button "Delete #{article.title}"
-
-    expect(page).to_not have_content(article.title)
-    expect(page).to have_content("Sign Out")
-  end
-
-  xit "an unregistered user can see all articles" do
-    expect(page).to have_content("View All Articles")
-
-    click_link "View All Articles"
 
     expect(page).to have_content(article.title)
     expect(page).to have_content("Sign Out")
   end
 
-  xit "a user cannot post articles" do
+  scenario "a registered user can see an individual article" do
+    visit root_path
+    sign_in(user)
+
+    expect(page).to have_content("Hey, nice to see you again.")
     expect(page).to have_content("View All Articles")
+    expect(page).to have_content("Sign Out")
+
+    click_link "View All Articles"
+    expect(page).to have_content(article.title)
+
+    visit article_path(article)
+    expect(page).to have_content(article.body)
+  end
+
+  scenario "an unregistered user can see all articles" do
+    visit root_path
+
+    expect(page).to have_content("View All Articles")
+    expect(page).to_not have_content("Sign Out")
+    expect(page).to_not have_content("Hey, nice to see you again.")
 
     click_link "View All Articles"
 
     expect(page).to have_content(article.title)
+  end
+
+  scenario "a unregistered user can see an individual article" do
+    visit root_path
+
+    expect(page).to have_content("View All Articles")
+    expect(page).to_not have_content("Sign Out")
+    expect(page).to_not have_content("Hey, nice to see you again.")
+
+    click_link "View All Articles"
+    expect(page).to have_content(article.title)
+
+    visit article_path(article)
+    expect(page).to have_content(article.body)
+  end
+
+  scenario "a user cannot post articles" do
+    visit root_path
+    sign_in(user)
+
+    expect(page).to have_content("View All Articles")
+    expect(page).to have_content("Sign Out")
+    expect(page).to have_content("Hey, nice to see you again.")
     expect(page).to_not have_content("Post New Article")
-    expect(page).to have_content("Sign Out")
   end
 
-  xit "a user cannot edit articles" do
+  scenario "a user cannot edit articles" do
+    visit root_path
+    sign_in(user)
+
     expect(page).to have_content("View All Articles")
+    expect(page).to have_content("Sign Out")
+    expect(page).to have_content("Hey, nice to see you again.")
 
     click_link "View All Articles"
 
     expect(page).to have_content(article.title)
     expect(page).to_not have_content("Edit #{article.title}")
-    expect(page).to have_content("Sign Out")
   end
 
-  xit "a user cannot delete articles" do
+  scenario "a user cannot delete articles" do
+    visit root_path
+    sign_in(user)
+
     expect(page).to have_content("View All Articles")
+    expect(page).to have_content("Sign Out")
+    expect(page).to have_content("Hey, nice to see you again.")
 
     click_link "View All Articles"
 
     expect(page).to have_content(article.title)
     expect(page).to_not have_content("Delete #{article.title}")
-    expect(page).to have_content("Sign Out")
   end
 end
